3b7cfca7327b3a487beae866eff58d89fcb3452d
rxttenxo <sergejproff19@gmail.com>
2023-12-20 18:13:56 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+//import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HairStylist());
+}
+
+class HairStylist extends StatelessWidget {
+  const HairStylist({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: const Icon(Icons.arrow_back_ios),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search_rounded,
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Hair Stylist',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 220,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 220,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
